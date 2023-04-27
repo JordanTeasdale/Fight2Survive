fba73f54802a60a6807d498f7f9b0de730612e13
Vivid-Hornet <85631937+JordanTeasdale@users.noreply.github.com>
2023-04-27 00:28:10 -0400

    Added character rotation (needs to be fixed)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,14 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    [SerializeField] LayerMask layersToHit;
+
     [Header("-----Components-----")]
 
     [SerializeField] CharacterController controller;
+    public GameObject cameraMain;
+    [SerializeField] GameObject camRotPoint;
 
     [Header("-----Player Attributes-----")]
 
@@ -21,6 +24,10 @@ public class PlayerController : MonoBehaviour
     Vector3 playerVelocity;
     Vector3 move = Vector3.zero;
 
+    Vector3 screenPosition;
+    Vector3 worldPosition;
+    Vector3 mouseDir;
+
     float playerSpeedOriginal;
     public int HPOrig;
     int prevHP;
@@ -32,19 +39,16 @@ public class PlayerController : MonoBehaviour
     bool isSprinting = false;
     bool isMeleeing = false;
 
-    public GameObject cameraMain;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         playerSpeedOriginal = playerSpeed;
         HPOrig = HP;
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (!isDead)
             PlayerMovement();
     }
@@ -58,14 +62,29 @@ public class PlayerController : MonoBehaviour
     }
 
     void PlayerMovement() {
+        screenPosition = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
 
-            //Getting input from Unity Input Manager
-            move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+        if (Physics.Raycast(ray, out RaycastHit hitData, 10000, layersToHit)) {
+            worldPosition = hitData.point;
+            Debug.Log("Hitting");
+        }
+        Debug.DrawLine(cameraMain.transform.position, worldPosition);
 
-            //Adding the move vector to the character controller
-            controller.Move(move * playerSpeed * Time.deltaTime);
+        mouseDir = worldPosition - transform.position;
 
-            controller.Move(playerVelocity * Time.deltaTime);
+        float angle = Vector3.Angle(new Vector3(transform.forward.x, 0, transform.forward.z), new Vector3(mouseDir.x, 0, mouseDir.z));
+        if (angle > 2)
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + angle, transform.localEulerAngles.z); 
+        //camRotPoint.transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + angle, transform.rotation.z);
+
+        //Getting input from Unity Input Manager
+        move = (new Vector3(1, 0, 0) * Input.GetAxis("Horizontal")) + (new Vector3(0, 0, 1) * Input.GetAxis("Vertical"));
+
+        //Adding the move vector to the character controller
+        controller.Move(move * playerSpeed * Time.deltaTime);
+
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 
     public void Respawn() {
